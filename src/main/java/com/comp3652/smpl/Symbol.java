6faef5a538e4d1a363096aa014c6a6c7722aad6b
maschad <chad.nehemiah94@gmail.com>
2015-12-16 19:04:04 -0500

    finished with exception of allowance for comments

--- src/main/java/com/comp3652/smpl/Symbol.java
+++ src/main/java/com/comp3652/smpl/Symbol.java
@@ -8,63 +8,61 @@ package com.comp3652.smpl.lang;
 /** CUP generated interface containing symbol constants. */
 public interface Symbol {
   /* terminals */
-  public static final int IFEQUIVALENT = 22;
-  public static final int SUBSTRING = 25;
-  public static final int LBRACKET = 26;
-  public static final int CHAR = 45;
-  public static final int READINT = 19;
-  public static final int IFPAIR = 13;
-  public static final int CASE = 20;
-  public static final int CAR = 6;
-  public static final int INTEGER = 43;
-  public static final int CDR = 7;
-  public static final int LPAREN = 28;
-  public static final int SEMI = 33;
-  public static final int PRINTLN = 17;
-  public static final int MINUS = 38;
-  public static final int CMP = 48;
-  public static final int RPAREN = 29;
-  public static final int NOT = 12;
-  public static final int AND = 10;
-  public static final int IN = 4;
-  public static final int OR = 11;
-  public static final int COMMA = 30;
-  public static final int LAZY = 9;
-  public static final int CALL = 15;
-  public static final int DIV = 40;
-  public static final int PLUS = 37;
-  public static final int ASSIGN = 42;
-  public static final int IF = 5;
+  public static final int IFEQUIVALENT = 21;
+  public static final int SUBSTRING = 24;
+  public static final int LBRACKET = 25;
+  public static final int CHAR = 44;
+  public static final int READINT = 18;
+  public static final int IFPAIR = 12;
+  public static final int CASE = 19;
+  public static final int CAR = 5;
+  public static final int INTEGER = 42;
+  public static final int CDR = 6;
+  public static final int LPAREN = 27;
+  public static final int SEMI = 32;
+  public static final int PRINTLN = 16;
+  public static final int MINUS = 37;
+  public static final int CMP = 47;
+  public static final int RPAREN = 28;
+  public static final int NOT = 11;
+  public static final int AND = 9;
+  public static final int OR = 10;
+  public static final int COMMA = 29;
+  public static final int LAZY = 8;
+  public static final int CALL = 14;
+  public static final int DIV = 39;
+  public static final int PLUS = 36;
+  public static final int ASSIGN = 41;
+  public static final int IF = 4;
   public static final int EOF = 0;
-  public static final int RBRACKET = 27;
-  public static final int TRUE = 46;
-  public static final int SIZE = 21;
+  public static final int RBRACKET = 26;
+  public static final int TRUE = 45;
+  public static final int SIZE = 20;
   public static final int error = 1;
-  public static final int MUL = 39;
-  public static final int MOD = 41;
-  public static final int PAIR = 14;
-  public static final int COLON = 34;
-  public static final int ELSE = 36;
-  public static final int LBRACE = 31;
-  public static final int VARIABLE = 49;
-  public static final int READ = 18;
-  public static final int LIST = 24;
-  public static final int FLOAT = 44;
+  public static final int MUL = 38;
+  public static final int MOD = 40;
+  public static final int PAIR = 13;
+  public static final int COLON = 33;
+  public static final int ELSE = 35;
+  public static final int LBRACE = 30;
+  public static final int VARIABLE = 48;
+  public static final int READ = 17;
+  public static final int LIST = 23;
+  public static final int FLOAT = 43;
   public static final int LET = 3;
-  public static final int THEN = 35;
-  public static final int RBRACE = 32;
-  public static final int IFEQUAL = 23;
-  public static final int STRING = 50;
-  public static final int PROC = 8;
+  public static final int THEN = 34;
+  public static final int RBRACE = 31;
+  public static final int IFEQUAL = 22;
+  public static final int STRING = 49;
+  public static final int PROC = 7;
   public static final int DEF = 2;
-  public static final int FALSE = 47;
-  public static final int PRINT = 16;
+  public static final int FALSE = 46;
+  public static final int PRINT = 15;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
   "DEF",
   "LET",
-  "IN",
   "IF",
   "CAR",
   "CDR",
