33e37a7bda02b2641202cc4b97d89df8f35ea638
shane <shane.richards121@gmail.com>
2015-12-20 20:21:53 -0500

    more lang classes

--- src/main/java/com/comp3652/lang/CIRVisitor.java
+++ src/main/java/com/comp3652/lang/CIRVisitor.java
@@ -4,6 +4,8 @@ import com.comp3652.sys.SMPLException;
 
 public interface CIRVisitor<S, T> extends ASTVisitor<CIRExp, S, T> {
 
-	public T visitCIRExp(CIRExp exp, S state) throws SMPLException;
-	
+	@Override
+	public Boolean visitCIRExp(CIRExp exp, S state) throws SMPLException {
+
+	}
 }
