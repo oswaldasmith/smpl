79d1c99d2844c4b0f08811c57928ec05a2275367
maschad <chad.nehemiah94@gmail.com>
2015-12-23 23:45:05 -0500

    read and print statement fixed

--- src/main/java/com/comp3652/lang/SMPLPrintStmt.java
+++ src/main/java/com/comp3652/lang/SMPLPrintStmt.java
@@ -6,13 +6,31 @@ import com.comp3652.sys.SMPLException;
  * Created by carlos on 12/20/15.
  */
 public class SMPLPrintStmt extends SMPLStatement {
-    public SMPLPrintStmt(ASTExp<AIRExp> e) {
-        //#TODO
+    private ASTExp<SMPLExp> exp;
+    private ASTExp<AIRExp> arithExp;
+    private boolean isPrintln;
+
+    public SMPLPrintStmt(SMPLExp e, boolean b) {
+        this.exp = e;
+        this.isPrintln = b;
     }
 
+    public SMPLPrintStmt(ASTExp<AIRExp> e, boolean b) {
+        this.arithExp = e;
+        this.isPrintln = b;
+    }
 
     @Override
     public <S, T> T visit(SMPLVisitor<S, T> v, S state) throws SMPLException {
         return v.visitSMPLPrintStmt(this,state);
+    }
+
+    public ASTExp<AIRExp> getArithExp() {
+        return arithExp;
+    }
+
+
+    public ASTExp<SMPLExp> getExp() {
+        return exp;
     }
 }
