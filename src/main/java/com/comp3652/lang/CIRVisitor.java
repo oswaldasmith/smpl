2cb102547995ade3fd7feb2e5e4091fd8668d76e
shane <shane.richards121@gmail.com>
2015-12-29 16:20:32 -0500

    no change

--- src/main/java/com/comp3652/lang/CIRVisitor.java
+++ src/main/java/com/comp3652/lang/CIRVisitor.java
@@ -3,5 +3,7 @@ package com.comp3652.lang;
 import com.comp3652.sys.SMPLException;
 
 public interface CIRVisitor<S, T> extends ASTVisitor<CIRExp, S, T> {
+
 	public T visitCIRExp(CIRExp exp, S state) throws SMPLException;
+
 }
